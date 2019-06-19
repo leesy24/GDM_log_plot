6c63acbbea6809178ee468ece1a0c32d73869415
Sang-young Lee <leesy24@gmail.com>
2019-06-19 18:54:32 +0900

    add to check input GDM log file is correct or not

--- GDM_log_plot.bat
+++ GDM_log_plot.bat
@@ -1,19 +1,30 @@
 @echo off
-if "%~1"=="" goto ERROR
+if "%~1"=="" goto ERROR_PARAM
 rem echo Current directory: %cd%
 rem echo Batch file's directory: %~dp0
+
 echo Preparing data from %1 ...
 findstr $PASHR %1 > %~dp0\log.dat
+for %%A in ("%~dp0\log.dat") do if %%~zA==0 goto ERROR_LOG_FILE
+
 echo Plotting data, wait a moment ...
 gnuplot --persist -c %~dp0\GDM_log_plot.plt %1 %~dp0\log.dat
+
 echo Plot exit.
 del %~dp0\log.dat
 rem pause
 exit
 
-:ERROR
+:ERROR_PARAM
+echo =====================================================
+echo You need to drag and drop the log file on this batch.
+echo =====================================================
+timeout 30 > NUL
+exit /b -1
+
+:ERROR_LOG_FILE
 echo ====================================================
-echo You need to drag and drop the log file on this batch
+echo You need to check drop file is correct GDM log file.
 echo ====================================================
 timeout 30 > NUL
 exit /b -1
